68ea7198fd0fb7a0d2432e2ef09a84833d8628f4
Pavel Shuvalov <shuvalov.pavel@gmail.com>
2023-07-26 16:49:24 +0300

    Production API endpoint

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,1 @@
-export const API_URL = 'http://localhost:8082'
+export const API_URL = process.env.REACT_APP_API_URL ?? 'https://tonalytica.redoubt.online/dex-optimiser/'  // 'http://localhost:8082'
