7b1d456e6d263c28d6379b6df471c9f41347841b
Pavel Shuvalov <shuvalov.pavel@gmail.com>
2023-07-27 17:17:29 +0300

    revert path

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: '/',
+  base: '/twa/',
 });
