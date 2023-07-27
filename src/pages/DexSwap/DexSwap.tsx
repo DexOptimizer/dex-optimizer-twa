07eac2151e207c0c32d3c791dbcb085469b552ea
manylovv <yellowmanylov@gmail.com>
2023-07-28 00:52:43 +0600

    improve ui of dex swap page

--- src/pages/DexSwap/DexSwap.tsx
+++ src/pages/DexSwap/DexSwap.tsx
@@ -1,102 +1,147 @@
-import './DexSwap.scss'
-import { Balance, Button } from '../../components/UI'
-import arrowReverse from '../../images/arrow-revers.png'
-import box from '../../images/box.png'
-import { toncoin } from '../../images'
-import { useNavigate } from 'react-router-dom'
-import { KEY_ROUTE_KEY } from '../../store/StorageKeys'
-import { Input } from '../../components/new-ui/input'
-import { cn } from '../../utils'
+import { Balance, Button } from '../../components/UI';
+import arrowReverse from '../../images/arrow-revers.png';
+import box from '../../images/box.png';
+import { toncoin } from '../../images';
+import { useNavigate } from 'react-router-dom';
+import { KEY_ROUTE_KEY } from '../../store/StorageKeys';
+import { Input } from '../../components/new-ui/input';
+import { cn } from '../../utils';
+import { MoveLeft, MoveRight } from 'lucide-react';
 
 const DexSwap = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
-
-    const route_raw = localStorage.getItem(KEY_ROUTE_KEY);
-    if (!route_raw) {
-        return (<Button className={'button'} onClick={() => navigate(-1)}>Back</Button>)
-    }
-    const route = JSON.parse(route_raw)
-    console.log(route);
-
-    function formatValue(v: number) {
-        if (!v) {
-            return '0';
-        }
-        return v.toLocaleString('en-US', {
-            // minimumIntegerDigits: 3,
-            // minimumFractionDigits: 2,
-            // maximumSignificantDigits: 2,
-            maximumFractionDigits: 2,
-            minimumFractionDigits: 2,
-            useGrouping: false
-        })
-    }
-
+  const route_raw = localStorage.getItem(KEY_ROUTE_KEY);
+  if (!route_raw) {
     return (
-        <>
-            {/* <div>
-                <pre>
-                    {JSON.stringify(route, undefined, " ")}
-                </pre>
-            </div> */}
-            <div className="header_optimize">
-                <div className='left'>
-                </div>
-                <Balance />
-            </div>
-            <div className='main'>
-                <span style={{ fontSize: '20px' }}>Best output:</span>
-                <div className='main_output'>
-                    <span style={{ backgroundColor: '', padding: '10px 10px 10px 10px' }}
-                        className={cn(
-                            'flex h-10 w-full rounded-xl border border-gray-800 bg-gray-900 px-2.5 py-2 text-sm outline-none focus:ring-1 focus:ring-sky-500 disabled:cursor-not-allowed disabled:opacity-50')}
-                    >{formatValue(route['src_value'])}&nbsp;{route['src_token']}</span>
-                    =&gt;&nbsp;
-                    <span style={{ backgroundColor: '', padding: '10px 10px 10px 10px' }}
-                        className={cn(
-                            'flex h-10 w-full rounded-xl border border-gray-800 bg-gray-900 px-2.5 py-2 text-sm outline-none focus:ring-1 focus:ring-sky-500 disabled:cursor-not-allowed disabled:opacity-50')}
-                    >{formatValue(route['output'])}&nbsp;{route['dst_token']}</span>
-                    {/* <DropDown/> */}
-                </div>
-                {
-                    (route['steps'] as []).map((step, index) => (
-                        <div className='step' key={index}>
-                            <div style={{ marginTop: '20px' }}>Steps {index + 1}</div>
-                            <img src={`https://redoubt.online/share/tondata/icons/${step['platform']}.png`}
-                                className={cn(
-                                    'h-10 w-10 select-none rounded-full bg-white p-px'
-                                )} />
-                            <div className={cn('flex w-full h-10')}>
-                                <span style={{ backgroundColor: '', padding: '10px 10px 10px 10px' }}
-                                    className={cn(
-                                        'flex h-10 w-full rounded-xl border border-gray-800 bg-gray-900 px-2.5 py-2 text-sm outline-none focus:ring-1 focus:ring-sky-500 disabled:cursor-not-allowed disabled:opacity-50')}
-                                >{formatValue(step['src_amount'])}&nbsp;{step['src']}</span>=&gt;&nbsp;
-                                <span style={{ backgroundColor: '', padding: '10px 10px 10px 10px' }}
-                                    className={cn(
-                                        'flex h-10 w-full rounded-xl border border-gray-800 bg-gray-900 px-2.5 py-2 text-sm outline-none focus:ring-1 focus:ring-sky-500 disabled:cursor-not-allowed disabled:opacity-50')}
-                                >{formatValue(step['dst_amount'])}&nbsp;{step['dst']}</span>
-                                <a
-                                    onClick={() => {
-                                        window.Telegram?.WebApp?.openLink(step['swap_url'], { try_instant_view: true });
-                                    }}
-                                    target="_blank"
-                                    className="float-right rounded-xl bg-sky-500 px-3.5 py-2 font-medium"
-                                >
-                                    Swap
-                                </a>
-                                {/* <Button className={'button'}>Swap</Button> */}
-                            </div>
+      <Button className={'button'} onClick={() => navigate(-1)}>
+        Back
+      </Button>
+    );
+  }
+  const route = JSON.parse(route_raw);
+  console.log(route);
 
+  function formatValue(v: number) {
+    if (!v) {
+      return '0';
+    }
+    return v.toLocaleString('en-US', {
+      // minimumIntegerDigits: 3,
+      // minimumFractionDigits: 2,
+      // maximumSignificantDigits: 2,
+      maximumFractionDigits: 2,
+      minimumFractionDigits: 2,
+      useGrouping: false,
+    });
+  }
+
+  return (
+    <div className="p-4 h-screen">
+      {/* <div>
+        <pre>{JSON.stringify(route, undefined, ' ')}</pre>
+      </div> */}
+
+      <div className="flex justify-between">
+        <Button
+          onClick={() => navigate(-1)}
+          className="flex items-center gap-2"
+        >
+          <MoveLeft />
+          Back
+        </Button>
+        <Balance />
+      </div>
+
+      <div className="flex flex-col justify-center h-[80%]">
+        <div className="flex flex-col gap-4">
+          <div>
+            <span className="text-sm">Best output</span>
+            <div className="flex gap-3">
+              <div className="flex h-12 w-full rounded-2xl items-center bg-gray-900 px-2.5 py-2">
+                <div className="flex items-baseline gap-1">
+                  <span className="text-xl font-semibold">
+                    {formatValue(route['src_value'])}
+                  </span>{' '}
+                  <span className="text-sm">{route['src_token']}</span>
+                </div>
+              </div>
+
+              <div className="flex h-12 w-full items-center rounded-2xl bg-gray-900 px-2.5 py-2">
+                <div className="flex items-baseline gap-1">
+                  <span className="font-semibold text-xl">
+                    {formatValue(route['output'])}
+                  </span>{' '}
+                  <span className="text-sm">{route['dst_token']}</span>
+                </div>
+              </div>
+              {/* <DropDown/> */}
+            </div>
+          </div>
+
+          <div>
+            {(route['steps'] as []).map((step, index) => (
+              <div key={index} className="flex flex-col gap-1">
+                <span className="text-sm">Step {index + 1}</span>
+                <div className="flex flex-col gap-1 bg-gray-900/70 rounded-2xl p-4">
+                  <div className="flex flex-col gap-4">
+                    <div className="flex gap-4 items-center">
+                      <img
+                        src={`https://redoubt.online/share/tondata/icons/${step['platform']}.png`}
+                        className="h-10 w-10 select-none rounded-full bg-white p-px"
+                      />
+
+                      <div className="flex justify-between items-center w-full">
+                        <div>
+                          <span className="text-sm">From</span>
+
+                          <div>
+                            <span className="text-xl font-semibold">
+                              {formatValue(step['src_amount'])}{' '}
+                            </span>
+
+                            <span className="text-sm font-normal">
+                              {step['src']}
+                            </span>
+                          </div>
                         </div>
-                    ))
-                }
 
+                        <MoveRight className="w-16" />
 
-                <Button className={'button'} onClick={() => navigate(-1)}>Back</Button>
-            </div>
-        </>
-    )
-}
+                        <div>
+                          <span className="text-sm">To</span>
 
-export default DexSwap
+                          <div>
+                            <span className="text-xl font-semibold">
+                              {formatValue(step['dst_amount'])}
+                            </span>{' '}
+                            <span className="text-sm font-normal">
+                              {step['dst']}
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <a
+                      onClick={() => {
+                        window.Telegram?.WebApp?.openLink(step['swap_url'], {
+                          try_instant_view: true,
+                        });
+                      }}
+                      target="_blank"
+                      className="rounded-xl justify-center bg-sky-500 px-3.5 h-10 flex items-center font-medium"
+                    >
+                      Swap
+                    </a>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DexSwap;
