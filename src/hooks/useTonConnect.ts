46c352b61ec9c42c3b563f1b4ae826c9c8fe7c50
Pavel Shuvalov <shuvalov.pavel@gmail.com>
2023-07-27 17:04:38 +0300

    Payment integration

--- src/hooks/useTonConnect.ts
+++ src/hooks/useTonConnect.ts
@@ -3,6 +3,7 @@ import { Sender, SenderArguments } from "ton-core";
 import { useTonConnectUI, useTonWallet } from "@tonconnect/ui-react";
 
 export function useTonConnect(): {
+  sender: Sender;
   connected: boolean;
   wallet: string | null;
   network: CHAIN | null;
@@ -11,6 +12,20 @@ export function useTonConnect(): {
   const wallet = useTonWallet();
 
   return {
+    sender: {
+      send: async (args: SenderArguments) => {
+        tonConnectUI.sendTransaction({
+          messages: [
+            {
+              address: args.to.toString(),
+              amount: args.value.toString(),
+              payload: args.body?.toBoc().toString("base64"),
+            },
+          ],
+          validUntil: Date.now() + 5 * 60 * 1000, // 5 minutes for user to approve
+        });
+      },
+    },
     connected: !!wallet?.account.address,
     wallet: wallet?.account.address ?? null,
     network: wallet?.account.chain ?? null,
