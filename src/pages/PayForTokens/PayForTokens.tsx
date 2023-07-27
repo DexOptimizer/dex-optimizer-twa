31ba01bdfc252a25ef4202dcb5f1ae13ea6b2659
manylovv <yellowmanylov@gmail.com>
2023-07-27 23:22:10 +0600

    improve ui of pay for tokens page

--- src/pages/PayForTokens/PayForTokens.tsx
+++ src/pages/PayForTokens/PayForTokens.tsx
@@ -1,4 +1,3 @@
-import './PayForTokens.scss';
 import { redoubt, toncoin } from '../../images/index';
 import { Button, Balance } from '../../components/UI/index';
 import { useNavigate } from 'react-router-dom';
@@ -10,6 +9,7 @@ import { useTonConnect } from '../../hooks/useTonConnect';
 import { Address, toNano } from 'ton';
 import { API_URL } from '../../api/api';
 import { toUserFriendlyAddress } from '@tonconnect/ui';
+import { MoveLeft } from 'lucide-react';
 
 const PayForTokens = () => {
   const [, updateState] = useState({});
@@ -49,71 +49,79 @@ const PayForTokens = () => {
   }
 
   return (
-    <>
-      <div className="header_PayForTokens">
+    <div className="p-4 h-screen">
+      <div className="flex justify-between">
+        <Button
+          onClick={() => navigate(-1)}
+          className="flex items-center gap-2"
+        >
+          <MoveLeft />
+          Back
+        </Button>{' '}
         <Balance />
       </div>
-      <div className="main">
-        <span>Amount of tokens you want to buy:</span>
-        <div className="Wrapper_position">
-          <Input
-            className="w-full text-lg h-12 font-semibold placeholder:text-gray-500"
-            type="number"
-            pattern="\d*"
-            placeholder="Amount"
-            onChange={(e) => {
-              if (e.target.value === '') {
-                setValue(0);
-              } else {
-                setValue(parseInt(e.target.value));
-              }
-              console.log(value);
-            }}
-          />
-          <img
-            src={redoubt}
-            style={{ width: '3rem', marginLeft: '10px' }}
-            alt=""
-          />
+
+      <div className="h-[80%] flex flex-col justify-center gap-4">
+        <div className="flex flex-col gap-1">
+          <label htmlFor="amount">Amount of tokens you want to buy</label>
+          <div className="flex items-center relative">
+            <Input
+              id="amount"
+              className="w-full text-lg h-12 font-semibold placeholder:text-gray-500"
+              type="number"
+              pattern="\d*"
+              placeholder="Amount"
+              onChange={(e) => {
+                if (e.target.value === '') {
+                  setValue(0);
+                } else {
+                  setValue(parseInt(e.target.value));
+                }
+                console.log(value);
+              }}
+            />
+            <img
+              src={redoubt}
+              className="h-8 absolute right-2"
+              alt="re:doubt logo"
+            />
+          </div>
         </div>
-        <span>=</span>
-        <div className="Wrapper_position">
-          <Input value={value / 10} disabled={true} />
+
+        <div className="flex relative items-center gap-1.5">
+          <Input
+            className="h-12 text-lg font-semibold border-none disabled:opacity-100"
+            value={value / 10}
+            disabled={true}
+          />
           <img
             src={toncoin}
-            style={{ width: '3rem', marginLeft: '10px' }}
-            alt=""
+            className="h-7 absolute right-2.5"
+            alt="TON logo"
           />
         </div>
-        <div className="wrapper_buttons">
-          <button
-            className={cn(
-              'rounded-2xl bg-sky-500 py-2.5 w-full font-medium transition duration-75 hover:bg-sky-400 focus:outline-none',
-              !connected || value <= 0 ? 'cursor-not-allowed opacity-50' : ''
-            )}
-            style={{ margin: '3px' }}
-            disabled={!connected || value <= 0}
-            onClick={handlePayment}
-          >
-            Pay
-          </button>
 
-          <button
-            style={{ margin: '3px' }}
-            className={cn(
-              'rounded-2xl bg-sky-500 py-2.5 w-full font-medium transition duration-75 hover:bg-sky-400 focus:outline-none'
-            )}
-            onClick={handleUpdate}
-          >
-            Check payment
-          </button>
+        <button
+          className={cn(
+            'rounded-2xl bg-sky-500 py-2.5 w-full font-medium transition duration-75 hover:bg-sky-400 focus:outline-none',
+            !connected || value <= 0 ? 'cursor-not-allowed opacity-50' : ''
+          )}
+          disabled={!connected || value <= 0}
+          onClick={handlePayment}
+        >
+          Pay
+        </button>
 
-          <Button className={'button'} onClick={() => navigate(-1)}>
-            Back
-          </Button>
-        </div>
+        <button
+          className={cn(
+            'rounded-2xl bg-sky-500 py-2.5 w-full font-medium transition duration-75 hover:bg-sky-400 focus:outline-none'
+          )}
+          onClick={handleUpdate}
+        >
+          Check payment
+        </button>
       </div>
-    </>
+    </div>
   );
 };
 export default PayForTokens;
