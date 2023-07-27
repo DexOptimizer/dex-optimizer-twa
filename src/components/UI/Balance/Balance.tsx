d2217ca809ba9e35c20d370f2e47a51006435e47
manylovv <yellowmanylov@gmail.com>
2023-07-27 20:37:54 +0600

    fix balance updating

--- src/components/UI/Balance/Balance.tsx
+++ src/components/UI/Balance/Balance.tsx
@@ -6,32 +6,30 @@ import { KEY_USER_KEY } from '../../../store/StorageKeys';
 import { useNavigate } from 'react-router-dom';
 import { RoutesName } from '../../../routes/constants';
 
-
 const Balance = () => {
-    const userId = localStorage.getItem(KEY_USER_KEY);
+  const userId = localStorage.getItem(KEY_USER_KEY);
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const [balance, setBalance] = useState(-1);
-    useEffect(() => {
-        fetch(API_URL + '/dexopt/api/v1/balance/' + userId).then(
-            async (value) => {
-                const response = await value.json();
-                setBalance(response['balance']);
-            }
-        );
-    }, [balance]);
+  const [balance, setBalance] = useState(-1);
 
-    if (balance == -1) {
-        // spinner ?
-        return null
-    }
+  useEffect(() => {
+    fetch(API_URL + '/dexopt/api/v1/balance/' + userId).then(async (value) => {
+      const response = await value.json();
+      setBalance(response['balance']);
+    });
+  });
 
-    return (
-        <div className="balance" onClick={() => navigate(RoutesName.PAYFORTOKENS)}>
-            <span id="service_fee">{balance}</span>
-            <img src={redoubt} style={{ width: '3rem' }} alt='' />
-        </div>
-    )
-}
-export default Balance
+  if (balance == -1) {
+    // spinner ?
+    return null;
+  }
+
+  return (
+    <div className="balance" onClick={() => navigate(RoutesName.PAYFORTOKENS)}>
+      <span id="service_fee">{balance}</span>
+      <img src={redoubt} style={{ width: '3rem' }} alt="" />
+    </div>
+  );
+};
+export default Balance;
